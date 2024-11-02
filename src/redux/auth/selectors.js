dd4bf5c96152eee5cb106b6c6b9b03d39213decb
Ira Prysiazhna <ira.prisyazhna@gmail.com>
2024-11-02 21:54:12 +0200

    async requests

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,3 +1,3 @@
-export const selectIsLoggedIn = state => state.authReducer.isLoggedIn;
-export const selectUser = state => state.authReducer.user;
-export const selectToken = state => state.authReducer.token;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+export const selectUser = state => state.auth.user;
+export const selectToken = state => state.auth.token;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,27 @@
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './auth/slice.js';
-
-// export const store = configureStore({
-//   reducer: {
-//     // Some code
-//   },
-// });
-import { createStore, combineReducers } from 'redux';
 import boardReducer from './reducers/boardReducer';
 import themeReducer from './reducers/themeReducer';
 import userReducer from './reducers/userReducer';
 
-const rootReducer = combineReducers({
-  board: boardReducer,
-  theme: themeReducer,
-  user: userReducer,
-  auth: authReducer,
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    board: boardReducer,
+    theme: themeReducer,
+    user: userReducer,
+  },
 });
 
-export const store = createStore(rootReducer);
+// import { createStore, combineReducers } from 'redux';
+// import boardReducer from './reducers/boardReducer';
+// import themeReducer from './reducers/themeReducer';
+// import userReducer from './reducers/userReducer';
+
+// const rootReducer = combineReducers({
+//   board: boardReducer,
+//   theme: themeReducer,
+//   user: userReducer,
+// });
+
+// export const store = createStore(rootReducer);
