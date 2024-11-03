3044025438ee01d47381472013ac3144be3ffedb
dr-jeky11 <tyrsyna@gmail.com>
2024-11-03 23:50:37 +0100

    hotfix header components positioning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,34 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import s from './Header.module.css'; 
-import { changeTheme } from '../../redux/actions/themeActions'; 
+import s from './Header.module.css';
+import { changeTheme } from '../../redux/actions/themeActions';
 import { FaBars } from 'react-icons/fa';
+import ThemeSwitcher from '../Themes/ThemeSwitcher/ThemeSwitcher';
 
 const Header = ({ toggleSidebar }) => {
   const dispatch = useDispatch();
-  const userName = useSelector((state) => state.user.name) || 'Guest';
-  const userPhoto = useSelector((state) => state.user.photo) || 'path/to/default-avatar.png'; 
+  const userName = useSelector(state => state.user.name) || 'Guest';
+  const userPhoto =
+    useSelector(state => state.user.photo) || 'path/to/default-avatar.png';
 
-  const handleThemeChange = (event) => {
+  const handleThemeChange = event => {
     dispatch(changeTheme(event.target.value));
   };
 
   return (
     <header className={s.header}>
       <div className={s.container}>
-        <div className={s.menuIcon} onClick={toggleSidebar}>
+        {/* <div className={s.menuIcon} onClick={toggleSidebar}>
           <FaBars />
-        </div>
-        <select onChange={handleThemeChange} className={s.themeSelector}>
-          <option value="light">Light</option>
-          <option value="dark">Dark</option>
-          <option value="violet">Violet</option>
-        </select>
-        <div className={s.userInfo}>
+        </div> */}
+        <ThemeSwitcher />
+        {/* <div className={s.userInfo}>
           <img src={userPhoto} alt={userName} className={s.userAvatar} />
           <span className={s.userName}>{userName}</span>
-        </div>
+        </div> */}
       </div>
     </header>
   );
 };
 
 export default Header;
-
