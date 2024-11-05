f7a22ca2c98ee7327837b09b8f83a5dafb29c78f
iahub4metra <buhai.artem7@gmail.com>
2024-11-05 23:52:58 +0000

    started making editModal

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,22 +1,50 @@
 import s from "./TaskCard.module.css"
 import sprite from "../../img/icons.svg"
+import { useState } from "react"
+import ReusableModal from "../Modal/Modal"
+import TaskForm from "../TaskForm/TaskForm"
+
+
+const taskObj = {
+    title: "Title card 1",
+    description: "decription card 1",
+    priority: "Low card 1",
+    deadline: "08/10/2024"
+}
+
+
+
 const TaskCard = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(true)
+    }
+
+    const handleClose = () => {
+        setIsOpen(false)
+        console.log("modal is closed");
+    }
+
+
+
     return (
         <>
             <div className={s.cardTask}>
-                <h4 className={s.taskTitle}>Title</h4>
-                <p className={s.taskDescription}>description</p>
+                <h4 className={s.taskTitle}>{taskObj.title}</h4>
+                <p className={s.taskDescription}>{taskObj.description}</p>
                 <div className={s.lowerCont}>
                     <div>
                         <div className={s.contWithMarkings}>
                             <p className={s.lowerContTitle}>Priority</p>
                             <div className={s.contWithCircle}>
-                                <span className={s.priorityCircle}></span><span className={s.lowerContText}>Low</span>
+                                <span className={s.priorityCircle}></span><span className={s.lowerContText}>{taskObj.priority}</span>
                             </div>
                         </div>
                         <div className={s.contWithMarkings}>
                             <p className={s.lowerContTitle}>Deadline</p>
-                            <p className={s.lowerContText}>Date</p>
+                            <p className={s.lowerContText}>{taskObj.deadline}</p>
                         </div>
                     </div>
                     <ul>
@@ -28,7 +56,7 @@ const TaskCard = () => {
                             </button>
                         </li>
                         <li>
-                            <button>
+                            <button onClick={()=> handleClick()}>
                                 <svg className={s.icon} width="16px" height="16px">
                                     <use href={`${sprite}#icon-pencil`}/>
                                 </svg>
@@ -44,7 +72,7 @@ const TaskCard = () => {
                     </ul>
                 </div>
             </div>
-        
+            <ReusableModal isOpen={isOpen} onClose={()=> handleClose()} children={<TaskForm initialTaskValue={taskObj} typeOfPopUp={'Edit'} />}/>
         </>
     );
 }
