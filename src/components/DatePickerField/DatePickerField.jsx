23d707bb435d2d840d42de326ee376afd4d72732
iahub4metra <buhai.artem7@gmail.com>
2024-11-07 12:31:12 +0000

    Update DatePickerField.jsx

--- src/components/DatePickerField/DatePickerField.jsx
+++ src/components/DatePickerField/DatePickerField.jsx
@@ -2,19 +2,23 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 import { FaChevronDown } from "react-icons/fa6";
 import s from "./DatePickerField.module.css"
-const DatePickerField = ({...props}) => {
+const DatePickerField = ({field, form, ...props}) => {
     return (
         <DatePicker
+            {...field}
             {...props}
+            selected={field.value}
+            selectedClassName={s.selectedDate}
+            onChange={(val) => form.setFieldValue(field.name, val)}
             showIcon
-            showPopperArrow={false}
             toggleCalendarOnIconClick
             icon={<FaChevronDown className={s.iconCalendar} />}
-            popperClassName={s.popperCustom}
             calendarClassName={s.customCal}
             dayClassName={date => s.dayCalendar}
             weekDayClassName={date => s.weekCalendar}
             weekClassName={date => s.week}
+            shouldCloseOnSelect={false}
+            disabledKeyboardNavigation
         />
     );
 }
