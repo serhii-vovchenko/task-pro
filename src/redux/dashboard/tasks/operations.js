371a606fb5e6d8be517044ece5dbcd9c3c225c84
iahub4metra <buhai.artem7@gmail.com>
2024-11-08 18:16:53 +0000

    Update operations.js

--- src/redux/dashboard/tasks/operations.js
+++ src/redux/dashboard/tasks/operations.js
@@ -8,8 +8,8 @@ export const deleteTask = createAsyncThunk('task/delete',
         const accessToken = state.auth.token
         try {
             setToken(accessToken)
-            const response = await api.delete(`/tasks/${taskId}`, taskId)
-            return response.data
+            await api.delete(`/tasks/${taskId}`)
+            return taskId
         } catch (error) {
             return thunkAPI.rejectWithValue(error.message);
         }
@@ -28,6 +28,24 @@ export const updateTask = createAsyncThunk('task/update',
         } catch (error) {
             console.log(error);
             return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+)
+
+
+export const createTask = createAsyncThunk('task/create',
+    async (values, thunkAPI) => {
+        const state = thunkAPI.getState()
+        const accessToken = state.auth.token
+
+        try {
+            setToken(accessToken)
+
+            const response = await api.post('/tasks', values)
+            return response.data
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(error.message)
         }
     }
 )
