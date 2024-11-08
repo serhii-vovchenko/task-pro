29e06265bcc36d53fa55784838255a7fa5d16593
iahub4metra <buhai.artem7@gmail.com>
2024-11-08 16:06:33 +0000

    Update TaskForm.jsx

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,5 +1,6 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import { format, isToday } from "date-fns";
+import * as Yup from "yup";
 import DatePickerField from "../DatePickerField/DatePickerField";
 import sprite from "../../img/icons.svg"
 import s from "./TaskForm.module.css"
@@ -7,9 +8,16 @@ import { useDispatch } from "react-redux";
 import { updateTask } from "../../redux/dashboard/tasks/operations";
 
 
+const validationSchema = Yup.object({
+  title: Yup.string().required("Title is required"),
+  description: Yup.string().required("Description is required"),
+  priority: Yup.string().required("Priority is required"),
+  deadline: Yup.date().nullable().required("Deadline is required")
+});
+
 const TaskForm = ({ initialTaskValue, typeOfPopUp }) => {
     const dispatch = useDispatch()
-    const taskId = initialTaskValue._id
+    const taskId = initialTaskValue?._id
     const handleSubmit = (values) => {
         const formData = {
             title: values.title,
@@ -17,7 +25,12 @@ const TaskForm = ({ initialTaskValue, typeOfPopUp }) => {
             priority: values.priority,
             deadline: format(values.deadline, "yyyy-MM-dd")
         }
-        dispatch(updateTask({taskId: taskId, data: formData}))
+        if (typeOfPopUp === "Edit") {
+            dispatch(updateTask({taskId: taskId, values: formData}))
+        } else {
+            console.log("add card");
+        }
+        
     }
 
     const formatDeadline = (date) => {
@@ -27,47 +40,53 @@ const TaskForm = ({ initialTaskValue, typeOfPopUp }) => {
     return (
         <div className={s.taskModal}>  
             <h3 className={s.titleModal}>{typeOfPopUp} Card</h3>
-            <Formik initialValues={initialTaskValue} onSubmit={handleSubmit}>
+            <Formik initialValues={initialTaskValue} onSubmit={handleSubmit} validationSchema={typeOfPopUp === "Add" ? validationSchema : null}>
                 {({ values, setFieldValue }) => (
                     <Form>
-                    <Field className={s.fieldTitle} type="text" name="title"/>
-                    <Field className={s.fieldDesc} type="text" as="textarea" name="description" />
-                    <div className={s.priorityCont}>
-                        <label className={s.labelTitle}>Label color</label>
-                        <ul>
-                            <li>
-                                <Field type="radio" name="priority" value="low"/>
-                                <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(143, 161, 208, 1)' }}></label>
-                            </li>
-                            <li>
-                                <Field type="radio" name="priority" value="medium"/>
-                                <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(224, 156, 181, 1)' }}></label>
-                            </li>
-                            <li>
-                                <Field type="radio" name="priority" value="high" />
-                                <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(190, 219, 176, 1)' }}></label>
-                            </li>
-                            <li>
-                                <Field type="radio" name="priority" value="none"/>
-                                <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(22, 22, 22, 0.3)' }}></label>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className={s.dateCont}>
+                        <div className={s.fieldCont}>
+                            <Field className={s.fieldTitle} type="text" name="title" />
+                            {typeOfPopUp === 'Add' && <ErrorMessage name="title" component="span" className={s.errorMsg} />}
+                        </div>
+                        <div className={s.textFieldCont}>
+                            <Field className={s.fieldDesc} type="text" as="textarea" name="description" />
+                            {typeOfPopUp === 'Add' && <ErrorMessage name="description" component="span" className={s.errorMsg} />}
+                        </div>
+                        <div className={s.priorityCont}>
+                            <label className={s.labelTitle}>Label color</label>
+                            <ul>
+                                <li>
+                                    <Field type="radio" name="priority" value="low"/>
+                                    <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(143, 161, 208, 1)' }}></label>
+                                </li>
+                                <li>
+                                    <Field type="radio" name="priority" value="medium"/>
+                                    <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(224, 156, 181, 1)' }}></label>
+                                </li>
+                                <li>
+                                    <Field type="radio" name="priority" value="high" />
+                                    <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(190, 219, 176, 1)' }}></label>
+                                </li>
+                                <li>
+                                    <Field type="radio" name="priority" value="none"/>
+                                    <label className={s.radioLabel} htmlFor="" style={{ '--customColor': 'rgba(22, 22, 22, 0.3)' }}></label>
+                                </li>
+                            </ul>
+                            {typeOfPopUp === 'Add' && <ErrorMessage name="priority" component="span" className={s.errorMsg} />}
+                        </div>
+                        <div className={s.dateCont}>
                             <label className={s.labelTitle} htmlFor="">Deadline</label>
                             <Field name="deadline" value={values.deadline ? formatDeadline(values.deadline) : "No date selected"} component={DatePickerField} minDate={new Date()} />
-                        
-                    </div>
-                        <button className={s.btnSubmit} type="submit">
-                            <span>
-                                <svg height='14px' width="14px">
-                                    <use href={`${sprite}#icon-plus`} />
-                                </svg>
-                            </span>
-                            
-                            {typeOfPopUp}
-                        </button>
-                </Form>
+                            {typeOfPopUp === 'Add' && <ErrorMessage name="deadline" component="span" className={s.errorMsg} />}
+                        </div>
+                            <button className={s.btnSubmit} type="submit">
+                                <span>
+                                    <svg height='14px' width="14px">
+                                        <use href={`${sprite}#icon-plus`} />
+                                    </svg>
+                                </span>
+                                {typeOfPopUp}
+                            </button>
+                    </Form>
                 )}
                 
             </Formik>
