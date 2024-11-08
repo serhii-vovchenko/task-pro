3e5fdbb2b451592ec1d7cced79cebb39c0c00d95
iahub4metra <buhai.artem7@gmail.com>
2024-11-08 11:02:21 +0000

    Update operations.js

--- src/redux/dashboard/tasks/operations.js
+++ src/redux/dashboard/tasks/operations.js
@@ -1,31 +1,33 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { api } from "../../../config/api";
-
-
-
-// export const getTasks = createAsyncThunk('/tasks',
-//     async (_, thunkAPI) => {
-//         try {
-            
-//         } catch (error) {
-            
-//         }
-//     }
-// )
+import { api, setToken } from "../../../config/api";
 
 export const deleteTask = createAsyncThunk('task/delete',
     async (taskId, thunkAPI) => {
-        try {
-            const state = thunkAPI.getState()
-            const token = state.auth.token
 
-            const {data} = await api.delete(`/tasks/${taskId}`, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            })
-            return data.data
+        const state = thunkAPI.getState()
+        const accessToken = state.auth.token
+        try {
+            setToken(accessToken)
+            const {response} = await api.delete(`/tasks/${taskId}`, taskId)
+            return response.data
         } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+)
+
+export const updateTask = createAsyncThunk('task/update',
+    async ({taskId, data}, thunkAPI) => {
+        const state = thunkAPI.getState()
+        const accessToken = state.auth.token
+        
+        try {
+            setToken(accessToken)
+            const { response } = await api.patch(`/tasks/${taskId}`, data)
+            console.log(response);
+            return response
+        } catch (error) {
+            console.log(error);
             return thunkAPI.rejectWithValue(error.message);
         }
     }
