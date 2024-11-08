509fc330ae2a94a4c1bbe0f783bd170a9457f2c2
iahub4metra <buhai.artem7@gmail.com>
2024-11-08 16:06:44 +0000

    Create AddCard.jsx

---
+++ src/components/AddCard/AddCard.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import sprite from "../../img/icons.svg"
+import ReusableModal from "../ReusableModal/ReusableModal";
+import TaskForm from "../TaskForm/TaskForm";
+import s from "./AddCard.module.css"
+
+const initialValue = {
+    title: "",
+    description: "",
+    priority: "none",
+    deadline: ""
+}
+
+const AddCard = () => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOpen = () => {
+        setIsOpen(true)
+    }
+
+    const handleClose = () => {
+        setIsOpen(false)
+    }
+
+    return (
+        <>
+            <button onClick={()=> handleOpen()} className={s.btnAddCard}>
+                <span className={s.btnInnerWrapper}>
+                    <span className={s.iconWrapper}>
+                        <svg width="14px" height="14px">
+                            <use href={`${sprite}#icon-plus`} />
+                        </svg>
+                    </span>
+                    <span>Add another card</span>
+                </span>
+            </button>
+            <ReusableModal isOpen={isOpen} onClose={() => handleClose()} children={<TaskForm initialTaskValue={initialValue} typeOfPopUp={'Add'}/>} />
+        </>
+    );
+}
+ 
+export default AddCard;
