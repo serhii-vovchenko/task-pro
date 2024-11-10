857fd388fe2c06c67e492df2312210a3a9b4dc61
iahub4metra <buhai.artem7@gmail.com>
2024-11-10 11:13:16 +0000

    Update TaskForm.jsx

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -2,10 +2,11 @@ import { ErrorMessage, Field, Form, Formik } from 'formik';
 import { format, isToday } from 'date-fns';
 import * as Yup from 'yup';
 import DatePickerField from '../DatePickerField/DatePickerField';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserTheme } from '../../redux/auth/selectors';
+import { createTask, updateTask } from '../../redux/dashboard/tasks/operations';
 import sprite from '../../img/icons.svg';
 import s from './TaskForm.module.css';
-import { useDispatch } from 'react-redux';
-import { createTask, updateTask } from '../../redux/dashboard/tasks/operations';
 
 const validationSchema = Yup.object({
   title: Yup.string().required('Title is required'),
@@ -17,7 +18,8 @@ const validationSchema = Yup.object({
 const TaskForm = ({ columnId, handleClose, initialTaskValue, typeOfPopUp }) => {
   const dispatch = useDispatch();
   const taskId = initialTaskValue?._id;
-
+  const theme = useSelector(selectUserTheme)
+  const color = theme === 'dark' ? "rgba(255, 255, 255, 0.3)" : "rgba(22, 22, 22, 0.3)"
   const handleSubmit = values => {
     const formData = {
       title: values.title,
@@ -101,7 +103,7 @@ const TaskForm = ({ columnId, handleClose, initialTaskValue, typeOfPopUp }) => {
                   <label
                     className={s.radioLabel}
                     htmlFor=""
-                    style={{ '--customColor': 'rgba(22, 22, 22, 0.3)' }}
+                    style={{ '--customColor': color }}
                   ></label>
                 </li>
               </ul>
