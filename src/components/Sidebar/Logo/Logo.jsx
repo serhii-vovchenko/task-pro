94025aa93ea20c01270baf328777abf61d8ce289
SergeyVovchenko <serega.vovchenko@gmail.com>
2024-11-10 19:27:25 +0100

    fix-logo-color

--- src/components/Sidebar/Logo/Logo.jsx
+++ src/components/Sidebar/Logo/Logo.jsx
@@ -1,11 +1,18 @@
+import { useSelector } from 'react-redux';
 import sprite from '../../../../src/img/icons.svg';
 import s from './Logo.module.css';
+import { selectUser } from '../../../redux/auth/selectors';
 
 const Logo = () => {
+  const user = useSelector(selectUser);
+
+  const logoIconSvg =
+    user.theme === 'violet' ? 'icon-logo-violet' : 'icon-logo';
+
   return (
     <div className={s.logoBox}>
       <svg className={s.logoIcon} height="32" width="32">
-        <use href={`${sprite}#icon-logo`} />
+        <use href={`${sprite}#${logoIconSvg}`} />
       </svg>
       <h2 className={s.title}>Task Pro</h2>
     </div>
