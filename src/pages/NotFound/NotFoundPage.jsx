ea336ffa6765793d645beedff0ce41cd2e439428
AndriyV2504 <andreyvolyanik@gmail.com>
2024-11-10 18:00:22 +0200

    NotFoundPage New

--- src/pages/NotFound/NotFoundPage.jsx
+++ src/pages/NotFound/NotFoundPage.jsx
@@ -4,10 +4,22 @@ import styles from './NotFoundPage.module.css';
 const NotFoundPage = () => {
   return (
     <div className={styles.notFoundContainer}>
-      <h1 className={styles.notFoundMessage}>Page Not Found</h1>
-      <Link to="/" className={styles.redirectButton}>
-        Go Back to Home
-      </Link>
+      <h1>PAGE NOT FOUND</h1>
+      <p className={styles.zoomArea}>
+        <b>Oops!</b> Sorry, we can't find that page!
+      </p>
+      <section className={styles.errorContainer}>
+        <span>4</span>
+        <span>
+          <span className={styles.screenReaderText}>0</span>
+        </span>
+        <span>4</span>
+      </section>
+      <div className={styles.linkContainer}>
+        <Link to="/" className={styles.moreLink}>
+          Go back to Home
+        </Link>
+      </div>
     </div>
   );
 };
