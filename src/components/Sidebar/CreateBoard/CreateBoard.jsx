d7095304d12b39c9f8707d12c9b0d8a1e3470df9
dr-jeky11 <tyrsyna@gmail.com>
2024-11-11 02:22:26 +0100

    validation board forms

--- src/components/Sidebar/CreateBoard/CreateBoard.jsx
+++ src/components/Sidebar/CreateBoard/CreateBoard.jsx
@@ -1,6 +1,8 @@
 import s from './CreateBoard.module.css';
 import { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 import sprite from '../../../img/icons.svg';
 import {
@@ -12,23 +14,33 @@ import { backgrounds } from '../../../../public/db/backgrounds.js';
 
 import SvgIcon from '../../SvgIcon/SvgIcon';
 
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, 'Too short!')
+    .max(70, 'Too Long!')
+    .required('This field is required!'),
+});
+
 export const NewBoard = ({ closeModal }) => {
+  const dispatch = useDispatch();
+
   const [iconsSelected, setIconsSelected] = useState(
     icons[0]?.name || '1_icon-project'
   );
-
   const [backgroundSelected, setBackgroundSelected] = useState('bg-0');
-  const [title, setTitle] = useState('');
   const modalRef = useRef(null);
 
-  const dispatch = useDispatch();
-
-  const handleTitleChange = event => setTitle(event.target.value);
-  const handleIconChange = event => {
-    setIconsSelected(event.currentTarget.dataset.source);
+  const handleIconChange = (event, setFieldValue) => {
+    const newIcon = event.currentTarget.dataset.source;
+    setIconsSelected(newIcon);
+    setFieldValue('iconName', newIcon);
   };
-  const handleBackgroundChange = event =>
-    setBackgroundSelected(event.currentTarget.dataset.source);
+
+  const handleBackgroundChange = (event, setFieldValue) => {
+    const newBackground = event.currentTarget.dataset.source;
+    setBackgroundSelected(newBackground);
+    setFieldValue('backgroundName', newBackground);
+  };
 
   useEffect(() => {
     const handleClickOutside = event => {
@@ -44,15 +56,9 @@ export const NewBoard = ({ closeModal }) => {
     };
   }, [closeModal]);
 
-  const newBoardObject = {
-    title,
-    iconName: iconsSelected,
-    backgroundName: backgroundSelected,
-  };
-
-  const createNewBoard = async () => {
+  const createNewBoard = async values => {
     try {
-      await dispatch(addBoard(newBoardObject));
+      await dispatch(addBoard(values));
       closeModal();
       dispatch(getBoardThunk());
     } catch (error) {
@@ -76,73 +82,101 @@ export const NewBoard = ({ closeModal }) => {
           if (e.target === e.currentTarget) closeModal();
         }}
       >
-        <div className={s.divCard}>
-          <h2 className={s.textNew}>New board</h2>
-          <button onClick={closeModal}>
-            <svg className={s.iconClose} height="32" width="32">
-              <use href={`${sprite}#icon-x-close`} />
-            </svg>
-          </button>
-          <input
-            className={s.titleInput}
-            type="text"
-            placeholder="Title"
-            value={title}
-            onChange={handleTitleChange}
-          />
+        <Formik
+          initialValues={{
+            title: '',
+            iconName: iconsSelected,
+            backgroundName: backgroundSelected,
+          }}
+          onSubmit={createNewBoard}
+          validationSchema={validationSchema}
+        >
+          {({ values, setFieldValue }) => (
+            <Form>
+              <div className={s.divCard}>
+                <h2 className={s.textNew}>New board</h2>
+                <button onClick={closeModal}>
+                  <svg className={s.iconClose} height="32" width="32">
+                    <use href={`${sprite}#icon-x-close`} />
+                  </svg>
+                </button>
+                <div className={s.fieldCont}>
+                  <Field
+                    className={s.titleInput}
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                  />
+                  <ErrorMessage
+                    name="title"
+                    component="div"
+                    className={s.errorMsg}
+                  />
+                </div>
 
-          <h3 className={s.textIcons}>Icons</h3>
-          <ul className={s.listDarkIcons}>
-            {icons.map((icon, index) => (
-              <li key={index}>
-                <input
-                  type="radio"
-                  value={icon.name}
-                  name="icons"
-                  className={s.inputRad}
-                  data-source={icon.name}
-                  checked={iconsSelected === icon.name}
-                  onChange={handleIconChange}
-                />
-                <SvgIcon
-                  url={icon.iconUrl}
-                  active={iconsSelected === icon.name}
-                />
-              </li>
-            ))}
-          </ul>
-          <h3 className={s.textBackground}>Background</h3>
-          <ul className={s.listColorIcons}>
-            {reorderedBackgrounds.map((bg, index) => (
-              <li
-                key={index}
-                className={
-                  backgroundSelected === bg?.name
-                    ? s.listItemActive
-                    : s.listItem
-                }
-              >
-                <input
-                  type="radio"
-                  name="backgrounds"
-                  data-source={bg.name}
-                  className={s.inputBack}
-                  checked={backgroundSelected === bg.name}
-                  onChange={handleBackgroundChange}
-                />
-                <img src={bg.modalUrl} alt={bg.name} className={s.img_back} />
-              </li>
-            ))}
-          </ul>
-          <button className={s.mainButton} onClick={createNewBoard}>
-            <div className={s.plusBtnOff}>
-              <svg className={s.addBtnIcon} height="32" width="32">
-                <use href={`${sprite}#icon-plus`} />
-              </svg>
-            </div>
-            Create
-          </button>
-        </div>
+                <h3 className={s.textIcons}>Icons</h3>
+                <ul className={s.listDarkIcons}>
+                  {icons.map((icon, index) => (
+                    <li key={index}>
+                      <input
+                        type="radio"
+                        value={icon.name}
+                        name="iconName"
+                        className={s.inputRad}
+                        data-source={icon.name}
+                        checked={values.iconName === icon.name}
+                        onChange={event =>
+                          handleIconChange(event, setFieldValue)
+                        }
+                      />
+                      <SvgIcon
+                        url={icon.iconUrl}
+                        active={values.iconName === icon.name}
+                      />
+                    </li>
+                  ))}
+                </ul>
+                <h3 className={s.textBackground}>Background</h3>
+                <ul className={s.listColorIcons}>
+                  {reorderedBackgrounds.map((bg, index) => (
+                    <li
+                      key={index}
+                      className={
+                        values.backgroundName === bg?.name
+                          ? s.listItemActive
+                          : s.listItem
+                      }
+                    >
+                      <input
+                        type="radio"
+                        name="backgroundName"
+                        data-source={bg.name}
+                        className={s.inputBack}
+                        checked={values.backgroundName === bg.name}
+                        onChange={event =>
+                          handleBackgroundChange(event, setFieldValue)
+                        }
+                      />
+                      <img
+                        src={bg.modalUrl}
+                        alt={bg.name}
+                        className={s.img_back}
+                      />
+                    </li>
+                  ))}
+                </ul>
+                <button className={s.mainButton} type="submit">
+                  <div className={s.plusBtnOff}>
+                    <svg className={s.addBtnIcon} height="32" width="32">
+                      <use href={`${sprite}#icon-plus`} />
+                    </svg>
+                  </div>
+                  Create
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
